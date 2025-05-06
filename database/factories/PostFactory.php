d8ef6deb892b70e6ca94a9e36634117bb2c462bc
Mel Garcia <meloervy@gmail.com>
2025-05-06 23:06:03 +0800

    Initial Commit

---
+++ database/factories/PostFactory.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Post>
+ */
+class PostFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        $title = fake()->sentence();
+        return [
+            'image' => fake()->imageUrl(),
+            'title' => fake()->sentence(),
+            'slug' => \Illuminate\Support\Str::slug($title),
+            'content' => fake()->paragraph(5),
+        ];
+    }
+}
